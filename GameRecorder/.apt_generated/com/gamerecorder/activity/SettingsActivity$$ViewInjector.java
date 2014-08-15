9605d8a2247010402bf877acd2cb5089995fff3a
bitjjj <bitjjj@foxmail.com>
2014-08-15 15:57:53 +0800

    update

--- GameRecorder/.apt_generated/com/gamerecorder/activity/SettingsActivity$$ViewInjector.java
+++ GameRecorder/.apt_generated/com/gamerecorder/activity/SettingsActivity$$ViewInjector.java
@@ -7,6 +7,20 @@ import butterknife.ButterKnife.Finder;
 public class SettingsActivity$$ViewInjector {
   public static void inject(Finder finder, final com.gamerecorder.activity.SettingsActivity target, Object source) {
     View view;
+    view = finder.findRequiredView(source, 2131361803, "field 'deleteTeamButton' and method 'deleteTeamButton'");
+    target.deleteTeamButton = (android.widget.ImageButton) view;
+    view.setOnClickListener(
+      new android.view.View.OnClickListener() {
+        @Override public void onClick(
+          android.view.View p0
+        ) {
+          target.deleteTeamButton(p0);
+        }
+      });
+    view = finder.findRequiredView(source, 2131361801, "field 'teamListSpinner'");
+    target.teamListSpinner = (android.widget.Spinner) view;
+    view = finder.findRequiredView(source, 2131361806, "field 'teammemberSwipeListView'");
+    target.teammemberSwipeListView = (com.fortysevendeg.swipelistview.SwipeListView) view;
     view = finder.findRequiredView(source, 2131361804, "field 'teammemberNameEdit'");
     target.teammemberNameEdit = (android.widget.EditText) view;
     view = finder.findRequiredView(source, 2131361805, "field 'addTeammemeberButton' and method 'addTeammemberButton'");
@@ -19,8 +33,6 @@ public class SettingsActivity$$ViewInjector {
           target.addTeammemberButton(p0);
         }
       });
-    view = finder.findRequiredView(source, 2131361801, "field 'teamListSpinner'");
-    target.teamListSpinner = (android.widget.Spinner) view;
     view = finder.findRequiredView(source, 2131361802, "field 'addTeamButton' and method 'addTeamButton'");
     target.addTeamButton = (android.widget.ImageButton) view;
     view.setOnClickListener(
@@ -31,26 +43,14 @@ public class SettingsActivity$$ViewInjector {
           target.addTeamButton(p0);
         }
       });
-    view = finder.findRequiredView(source, 2131361806, "field 'teammemberSwipeListView'");
-    target.teammemberSwipeListView = (com.fortysevendeg.swipelistview.SwipeListView) view;
-    view = finder.findRequiredView(source, 2131361803, "field 'deleteTeamButton' and method 'deleteTeamButton'");
-    target.deleteTeamButton = (android.widget.ImageButton) view;
-    view.setOnClickListener(
-      new android.view.View.OnClickListener() {
-        @Override public void onClick(
-          android.view.View p0
-        ) {
-          target.deleteTeamButton(p0);
-        }
-      });
   }
 
   public static void reset(com.gamerecorder.activity.SettingsActivity target) {
+    target.deleteTeamButton = null;
+    target.teamListSpinner = null;
+    target.teammemberSwipeListView = null;
     target.teammemberNameEdit = null;
     target.addTeammemeberButton = null;
-    target.teamListSpinner = null;
     target.addTeamButton = null;
-    target.teammemberSwipeListView = null;
-    target.deleteTeamButton = null;
   }
 }
